5b1005b5ec7f74516627feb0ed00549fb60ca97d
Alexey Abramenko <a.abramenko@chita.ru>
2022-10-16 11:56:26 +0300

    Add. Second Steps

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NewsController
+class NewsController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response {
@@ -14,7 +15,17 @@ class NewsController
 
     #[Route('/news/{id}')]
     public function news(int $id = null) : Response {
-        return new Response("<h1>News: {$id}</h1>");
 
+        $list = [
+            ['title' => 'Header 1', 'body' => 'Any thing, more text and more text'],
+            ['title' => 'Header 2', 'body' => '1 Any thing, more text and more text'],
+            ['title' => 'Header 3', 'body' => '2 Any thing, more text and more text'],
+            ['title' => 'Header 4', 'body' => '3 Any thing, more text and more text']
+        ];
+
+        return $this->render('news/news-list.html.twig', [
+            'title' => 'Hello world',
+            'list' => $list
+        ]);
     }
 }
